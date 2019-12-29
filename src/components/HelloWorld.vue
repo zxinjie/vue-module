7bf7e6acf1e85954772f905e30f4b0a520989839
zouxinjie <zouxinjie110@126.com>
2019-12-29 23:03:23 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div @click="startPop">点击弹框</div>
+    <pop-win v-show="isShowPop"></pop-win>
+
+    <vui-swiper
+      height="171px"
+      :options="swiperOption"
+      :imageList="bannerList"
+      effect="zoom"
+      @onClick="handleSwiperClick"
+    ></vui-swiper>
+  </div>
+</template>
+
+<script>
+import vuiSwiper from "./vui-swiper";
+import PopWin from "./popWin";
+export default {
+  name: "HelloWorld",
+  components: {
+    PopWin,
+    vuiSwiper
+  },
+  data() {
+    return {
+      bannerList: [
+        {
+          img: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1480623082,1028728801&fm=26&gp=0.jpg'
+        },
+        {
+          img: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1062896970,1967996506&fm=26&gp=0.jpg"
+        },
+        {
+          img: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1088079182,2468727128&fm=26&gp=0.jpg"
+        }
+      ],
+      swiperOption: {
+        showDots: true, // 是否显示分页器
+        interval: 3000, // 轮播间隔时间，默认3s
+        autoplay: true, // 是否自动播放
+        loop: false // 是否循环轮播
+      },
+      isShowPop: false,
+    };
+  },
+  methods: {
+    handleSwiperClick() {},
+    startPop() {
+        this.isShowPop = !this.isShowPop
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/popWin.vue
@@ -0,0 +1,58 @@
+<template>
+  <transition name="fade">
+    <div class="pop-root">
+      <div class="pop-body">
+        <div class="pop-title">
+          <div></div>
+          <div></div>
+        </div>
+
+        <slot name="content"></slot>
+
+        <div class="pop-bottom"></div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'popWin',
+
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {}
+  };
+</script>
+
+<style lang="scss" scoped>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  .pop-root {
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.3);
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    justify-content: center;
+    align-items: center;
+    z-index: 2000;
+
+    .pop-body {
+      width: 80vw;
+      min-height: 30vh;
+      max-height: 75vh;
+      background: chocolate;
+      display: flex;
+    }
+  }
+
+
+</style>
